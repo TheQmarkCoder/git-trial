2ad7189ba7f9bba942c1457f57440ecd8f447036
TheQmarkCoder <girishmegam@gmail.com>
2025-01-18 11:30:00 +0530

    version 6

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 console.log('config.js');
 console.log('verison 1');
 console.log('version 2');
-console.log('version 5');
+console.log('version 6');
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 console.log('index.js');
 console.log('version 1');
 console.log('version 2');
-console.log('version 5');
+console.log('version 6');
